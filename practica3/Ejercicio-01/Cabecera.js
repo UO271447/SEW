32f79723be17eb0c94622256cf121731021321dd
Javier Lopez de Juan <79641878+UO271447@users.noreply.github.com>
2022-11-18 13:32:15 +0100

    Delete practica3 directory

--- practica3/Ejercicio-01/Cabecera.js
+++
@@ -1,8 +0,0 @@
-var asignatura = new Object();
-asignatura.nombre = "Software y Estándares para la Web (SEW)";
-asignatura.titulacion = "Grado ingeniería informática";
-asignatura.centro = "Escuela ingeniería informática";
-asignatura.universidad = "Universidad de Oviedo";
-asignatura.curso = "2022-23";
-asignatura.alumno = "Javier Lopez De Juan";
-asignatura.email = "UO271447@uniovi.es";
