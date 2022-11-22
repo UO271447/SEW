ddddcd84f7aed7c9faf6854f3f0d22982bd9ec50
Javier Lopez de Juan <79641878+UO271447@users.noreply.github.com>
2022-11-22 13:48:54 +0100

    Add files via upload

---
+++ practica3/Ejercicio-02/MasInfoNavegador.js
@@ -0,0 +1,15 @@
+document.write("<p>Version Navegador: ");
+document.write(navegador.version);
+document.write("</p>");
+document.write("<p>Plataforma: ");
+document.write(navegador.plataforma);
+document.write("</p>");
+document.write("<p>Vendedor: ");
+document.write(navegador.vendedor);
+document.write("</p>");
+document.write("<p>Agente: ");
+document.write(navegador.agente );
+document.write("</p>");
+document.write("<p>Java Activo: ");
+document.write(navegador.javaActivo);
+document.write("</p>");
