3483bd37d3cceb42966d960e99fb75d84489e0d6
Javier Lopez de Juan <79641878+UO271447@users.noreply.github.com>
2022-12-05 15:58:54 +0100

    cansao

---
+++ PRACTICA-3/Ejercicio-11/Tarea1/Ejercicio11.js
@@ -0,0 +1,34 @@
+
+"use strict";
+class Geolocalización {
+    constructor (){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+    }
+    getPosicion(posicion){
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+        this.altitud          = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo            = posicion.coords.heading;
+        this.velocidad        = posicion.coords.speed;       
+    }
+    getLongitud(){
+        return this.longitud;
+    }
+    getLatitud(){
+        return this.latitud;
+    }
+    getAltitud(){
+        return this.altitud;
+    }
+    informacion(dondeVerlo){
+        var ubicacion=document.getElementsByTagName('main')[0];
+        var datos=''; 
+        datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
+        datos+='<p>Latitud: '+this.latitud +' grados</p>';
+        datos+='<p>Precisión de la latitud y longitud: '+ this.precision +' metros</p>';
+        ubicacion.innerHTML = datos;
+    }
+}
+var posicion = new Geolocalización();
